e730fabc0244f44207f68d7baea82f21914a6399
Kong Chae Won <chaewonkong@gmail.com>
2019-11-30 22:44:34 +0900

    feat: add marker and infowindow

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,7 +1,9 @@
 /*global kakao*/
 
 import { IonPage, IonContent, IonHeader, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { konoAPI } from "../api"
+import { IKonoList } from "../shared-interfaces"
 
 declare global {
     interface Window {
@@ -12,6 +14,9 @@ declare global {
 
 
 const Map: React.FC = () => {
+
+    const [map, setMap] = useState()
+    const [konoList, setKonoList] = useState()
 
     useIonViewDidEnter(() => {
         if (window.kakao) {
@@ -24,9 +29,44 @@ const Map: React.FC = () => {
             let container = document.getElementById('map')
 
             let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-            if (container) map.relayout()
+            // if (container) map.relayout()
+            setMap(map)
+            getKonoList(37.1, 127.1)
         }
     })
+
+    useEffect(() => {
+        // 마커가 표시될 위치입니다 
+        if (konoList && map) {
+            konoList.map((place: IKonoList) => {
+                let { x, y, name } = place
+                console.log(name)
+                let latlng = new window.kakao.maps.LatLng(y, x)
+
+                let marker = new window.kakao.maps.Marker({
+                    map: map, // 마커를 표시할 지도
+                    position: latlng,
+                    title: name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                });
+
+                var infowindow = new window.kakao.maps.InfoWindow({
+                    position: latlng,
+                    content: `<div style="padding: 4px; ">${name}</div>`
+                });
+
+                // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+                infowindow.open(map, marker);
+                marker.setMap(map)
+            })
+        }
+    }, [konoList])
+
+    const getKonoList = async (x: number, y: number): Promise<void> => {
+        const data = await konoAPI.getKonoList(x, y)
+        setKonoList(data)
+    }
+
+    console.log(konoList)
 
     return (
         <IonPage>
@@ -43,9 +83,11 @@ const Map: React.FC = () => {
 }
 
 
+
 const mapStyle = {
     width: "100%",
     height: "100%"
 }
 
 export default Map
+
